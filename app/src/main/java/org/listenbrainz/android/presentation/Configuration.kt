ac455b38dca51f1046d3bfac34b7cffaef1afef8
Akshat Tiwari <tiwariakshat03@gmail.com>
2022-12-31 13:35:27 +0530

    Fix Spotify Remote Config
    - Updated the client id and did the setup for it on spotify developer console.
    - Fix the naming at places.

--- app/src/main/java/org/listenbrainz/android/presentation/Configuration.kt
+++ app/src/main/java/org/listenbrainz/android/presentation/Configuration.kt
@@ -2,10 +2,10 @@ package org.listenbrainz.android.presentation
 
 object Configuration {
     const val LIVE = false
-    const val TAG = "MusicBrainz"
+    const val TAG = "ListenBrainz"
     const val FEEDBACK_EMAIL = "support@metabrainz.org"
-    const val FEEDBACK_SUBJECT = "[MBAndroid] Feedback"
-    const val USER_AGENT = "MusicBrainzAndroid"
+    const val FEEDBACK_SUBJECT = "[LBAndroid] Feedback"
+    const val USER_AGENT = "ListenBrainzAndroid"
     const val CLIENT_NAME = "org.listenbrainz.android"
     private const val WEBSITE = "http://metabrainz.org/"
     const val URL_DONATE = "http://metabrainz.org/donate"
