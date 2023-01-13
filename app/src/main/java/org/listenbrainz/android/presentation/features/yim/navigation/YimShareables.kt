45b1cb2691acb5a649d2a7c08038e6ea5dc56215
Jasjeet Singh <07jasjeet@gmail.com>
2023-01-13 23:44:39 +0530

    Implemented enums, fixed viewpager fps drops

---
+++ app/src/main/java/org/listenbrainz/android/presentation/features/yim/navigation/YimShareables.kt
@@ -0,0 +1,10 @@
+package org.listenbrainz.android.presentation.features.yim.navigation
+
+enum class YimShareables(val code: String) {
+    Tracks("tracks"),
+    Statistics("stats"),
+    Artists("artists"),
+    Albums("albums"),
+    Discoveries("discovery-playlist"),
+    Undiscovered("missed-playlist")
+}
