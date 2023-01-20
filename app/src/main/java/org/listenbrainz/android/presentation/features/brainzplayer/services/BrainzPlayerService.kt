6a1ce98e8601a14a787f0688f5db9a0352c2c9f1
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2023-01-20 22:25:56 +0530

    Cleanup

--- app/src/main/java/org/listenbrainz/android/presentation/features/brainzplayer/services/BrainzPlayerService.kt
+++ app/src/main/java/org/listenbrainz/android/presentation/features/brainzplayer/services/BrainzPlayerService.kt
@@ -7,7 +7,6 @@ import android.support.v4.media.MediaBrowserCompat
 import android.support.v4.media.MediaDescriptionCompat
 import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaSessionCompat
-import android.util.Log
 import androidx.media.MediaBrowserServiceCompat
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.Player
