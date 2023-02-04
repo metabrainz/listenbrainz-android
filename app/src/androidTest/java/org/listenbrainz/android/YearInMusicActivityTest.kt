0957c445bda324fc303ac464ea8b650e712cbe65
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2023-02-04 12:29:29 +0530

    Added dependencies and UI Tests initialized

---
+++ app/src/androidTest/java/org/listenbrainz/android/YearInMusicActivityTest.kt
@@ -0,0 +1,32 @@
+package org.listenbrainz.android
+
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@HiltAndroidTest
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class YearInMusicActivityTest {
+    
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+    
+    @get:Rule(order = 1)
+    val rule = createComposeRule()
+    
+    @Before
+    fun init(){
+        hiltRule.inject()
+    }
+    
+    @Test
+    fun startupTest(){
+    }
+}
