552cc7e98dd86e00dc28b0c021db13fcbb5b916c
Sirius <pranav474738@gmail.com>
2024-01-04 22:31:55 +0530

    Use utils.parseResponse for getting yimData

--- app/src/main/java/org/listenbrainz/android/repository/yim23/Yim23RepositoryImpl.kt
+++ app/src/main/java/org/listenbrainz/android/repository/yim23/Yim23RepositoryImpl.kt
@@ -1,25 +1,24 @@
 package org.listenbrainz.android.repository.yim23
 
 import androidx.annotation.WorkerThread
+import org.listenbrainz.android.model.ResponseError
 import org.listenbrainz.android.model.yimdata.Yim23Payload
 import org.listenbrainz.android.model.yimdata.YimPayload
 import org.listenbrainz.android.service.Yim23Service
 import org.listenbrainz.android.service.YimService
 import org.listenbrainz.android.util.Log
 import org.listenbrainz.android.util.Resource
+import org.listenbrainz.android.util.Utils
+import retrofit2.http.GET
 import javax.inject.Inject
 
 class Yim23RepositoryImpl @Inject constructor(private val service: Yim23Service) : Yim23Repository {
 
     @WorkerThread
-    override suspend fun getYimData(username: String): Resource<Yim23Payload> {
-        return try {
-            val response = service.getYimData(username = username)
-            Resource(Resource.Status.SUCCESS, response)
-        }catch (e: Exception){
-            e.printStackTrace()
-            e.localizedMessage?.let { Log.w(it) }
-            Resource(Resource.Status.FAILED, null)
+    override suspend fun getYimData(username: String?): Resource<Yim23Payload> {
+        return Utils.parseResponse {
+            if(username == null) return ResponseError.AUTH_HEADER_NOT_FOUND.asResource()
+            service.getYimData(username)
         }
     }
 }
--- app/src/main/java/org/listenbrainz/android/service/Yim23Service.kt
+++ app/src/main/java/org/listenbrainz/android/service/Yim23Service.kt
@@ -3,6 +3,7 @@ package org.listenbrainz.android.service
 import dagger.Provides
 import org.listenbrainz.android.model.yimdata.Yim23Payload
 import org.listenbrainz.android.model.yimdata.YimPayload
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 import javax.inject.Singleton
@@ -12,7 +13,7 @@ interface Yim23Service {
 
     // https://api.listenbrainz.org/1/stats/user/(user_name)/year-in-music/2022
     @GET("stats/user/{user_name}/year-in-music/2023")
-    suspend fun getYimData(@Path("user_name") username: String): Yim23Payload
+    suspend fun getYimData(@Path("user_name") username: String): Response<Yim23Payload>
 
 }
 
