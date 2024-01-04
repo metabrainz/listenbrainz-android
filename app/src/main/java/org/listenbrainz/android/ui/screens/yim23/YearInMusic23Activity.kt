99ea428d6f2d26e1c5aab8816bd4ca9d74929506
Sirius <pranav474738@gmail.com>
2024-01-04 23:17:08 +0530

    Refactored YearInMusic23Activity file to have proper spacing and wrapped text

--- app/src/main/java/org/listenbrainz/android/ui/screens/yim23/YearInMusic23Activity.kt
+++ app/src/main/java/org/listenbrainz/android/ui/screens/yim23/YearInMusic23Activity.kt
@@ -24,7 +24,6 @@ class YearInMusic23Activity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val yim23ViewModel : Yim23ViewModel by viewModels()
         val socialViewModel : SocialViewModel by viewModels()
         val networkConnectivityViewModel: NetworkConnectivityViewModel =
@@ -34,10 +33,12 @@ class YearInMusic23Activity : ComponentActivity() {
             val loginStatus by yim23ViewModel.loginFlow.collectAsState(initial = STATUS_LOGGED_IN)
             // Login Check
             if (loginStatus == STATUS_LOGGED_OUT){
-                Toast.makeText(this, "Please Login to access your Year in Music!", Toast.LENGTH_LONG).show()
+                Toast.makeText(this, "Please Login to access your Year in Music!",
+                    Toast.LENGTH_LONG).show()
                 finish()
             }
-            Yim23Navigation(yimViewModel = yim23ViewModel, socialViewModel = socialViewModel,networkConnectivityViewModel = networkConnectivityViewModel, activity = this)
+            Yim23Navigation(yimViewModel = yim23ViewModel, socialViewModel = socialViewModel
+                , networkConnectivityViewModel = networkConnectivityViewModel, activity = this)
         }
     }
 }
