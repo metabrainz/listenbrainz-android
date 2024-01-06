d9348186c8bea6de91b75ca3f43b33ceee827dd3
Sirius <pranav474738@gmail.com>
2024-01-06 15:40:08 +0530

    Add swipe feature to base screen so that every screen has this functionality

--- app/src/main/java/org/listenbrainz/android/ui/screens/yim23/Yim23BaseScreen.kt
+++ app/src/main/java/org/listenbrainz/android/ui/screens/yim23/Yim23BaseScreen.kt
@@ -1,6 +1,7 @@
 package org.listenbrainz.android.ui.screens.yim23
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,7 +9,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.navigation.NavController
 import org.listenbrainz.android.model.yimdata.Yim23Screens
 import org.listenbrainz.android.ui.components.Yim23Footer
@@ -26,10 +31,30 @@ fun Yim23BaseScreen(
     content      : @Composable () -> Unit,
 ){
     val username by viewModel.getUsernameFlow().collectAsState(initial = "")
+    var swipeState by remember {
+        mutableStateOf(0)
+    }
     Yim23Theme(themeType = viewModel.themeType.value) {
         Column (modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.onBackground),
+            .background(MaterialTheme.colorScheme.onBackground)
+            .pointerInput(Unit) {
+                detectDragGestures { change, dragAmount ->
+                    val (x, y) = dragAmount
+                    if (y < 0) {
+                        if (swipeState == 0) {
+                            navController.navigate(route = downScreen.name)
+                            swipeState = 1
+                        }
+                    }
+                    else{
+                        if (swipeState == 0) {
+                            navController.popBackStack()
+                            swipeState = 1
+                        }
+                    }
+                }
+            },
                 verticalArrangement = Arrangement.SpaceBetween) {
             Yim23Header(username = username, navController = navController)
             content()
