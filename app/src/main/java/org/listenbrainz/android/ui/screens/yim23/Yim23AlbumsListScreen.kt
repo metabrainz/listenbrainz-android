f7522e6c19b890b3c07b6ce0bf9273646a5b8d5a
Sirius <pranav474738@gmail.com>
2024-01-07 12:30:04 +0530

    Made albums list uppercase , they are visible till end as in design

--- app/src/main/java/org/listenbrainz/android/ui/screens/yim23/Yim23AlbumsListScreen.kt
+++ app/src/main/java/org/listenbrainz/android/ui/screens/yim23/Yim23AlbumsListScreen.kt
@@ -1,16 +1,20 @@
 package org.listenbrainz.android.ui.screens.yim23
 
 
+import android.text.TextUtils.TruncateAt
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Text
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import org.listenbrainz.android.model.yimdata.TopReleaseYim23
@@ -37,8 +41,11 @@ fun Yim23AlbumsListScreen (
                     viewModel.getTopReleases() ?: listOf()
                 }
                 for(i in 1..5)
-                    Text(topReleases[i-1].releaseName ,
+                    Text(topReleases[i-1].releaseName.uppercase() ,
+                        modifier = Modifier
+                            .horizontalScroll(rememberScrollState() , false),
                         color = MaterialTheme.colorScheme.background ,
+                        overflow = TextOverflow.Visible,
                         style = MaterialTheme.typography.labelLarge , maxLines = 1)
             }
         }
