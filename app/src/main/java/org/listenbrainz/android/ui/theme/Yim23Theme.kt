3edfc61cba83b57faa9d4192df753962b8e37f5f
Sirius <pranav474738@gmail.com>
2024-01-11 18:16:12 +0530

    Add status bar and nav bar colors

--- app/src/main/java/org/listenbrainz/android/ui/theme/Yim23Theme.kt
+++ app/src/main/java/org/listenbrainz/android/ui/theme/Yim23Theme.kt
@@ -5,12 +5,14 @@ import android.util.Log
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
 import com.google.accompanist.systemuicontroller.SystemUiController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import kotlinx.coroutines.delay
 import org.listenbrainz.android.model.yimdata.Yim23ThemeData
 
 private val greenColorScheme = darkColorScheme(
@@ -50,6 +52,11 @@ fun Yim23Theme(
         Yim23ThemeData.GRAY -> greyColorScheme
         else -> greenColorScheme
     }
+
+    val view = LocalView.current
+    (view.context as Activity).window.statusBarColor = colorScheme.onBackground.toArgb()
+    (view.context as Activity).window.navigationBarColor = colorScheme.onBackground.toArgb()
+
 
 
     MaterialTheme(
