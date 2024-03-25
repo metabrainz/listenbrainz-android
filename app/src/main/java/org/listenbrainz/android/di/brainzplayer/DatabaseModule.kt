2e1cca421c7b0c036387d99321a307f6e0236fc6
Pranav <122373207+pranavkonidena@users.noreply.github.com>
2024-03-25 18:08:25 +0530

    Completed Room Migration

--- app/src/main/java/org/listenbrainz/android/di/brainzplayer/DatabaseModule.kt
+++ app/src/main/java/org/listenbrainz/android/di/brainzplayer/DatabaseModule.kt
@@ -2,12 +2,23 @@ package org.listenbrainz.android.di.brainzplayer
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+
+
+val MIGRATION_1_2: Migration = object : Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL(
+            "ALTER TABLE 'SONGS' ADD COLUMN 'lastListenedTo' INTEGER NOT NULL DEFAULT 0"
+        )
+    }
+}
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -21,6 +32,7 @@ object DatabaseModule {
         BrainzPlayerDatabase::class.java,
         "brainzplayer_database"
     )
+        .addMigrations(MIGRATION_1_2)
         .build()
     
     @Provides
