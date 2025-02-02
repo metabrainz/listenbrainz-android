d305ecfdd427e1a88f270a430bdfacb5963dbfea
rahul31124 <rdass87871@gmail.com>
2025-02-02 16:35:26 +0530

    Fix crash and improve performance in AlbumsOverViewScreen

--- app/src/main/java/org/listenbrainz/android/ui/screens/brainzplayer/overview/AlbumsOverviewScreen.kt
+++ app/src/main/java/org/listenbrainz/android/ui/screens/brainzplayer/overview/AlbumsOverviewScreen.kt
@@ -3,6 +3,7 @@ package org.listenbrainz.android.ui.screens.brainzplayer.overview
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -19,58 +20,62 @@ import org.listenbrainz.android.R
 import org.listenbrainz.android.model.Album
 import org.listenbrainz.android.ui.components.ListenCardSmall
 import org.listenbrainz.android.ui.theme.ListenBrainzTheme
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 
 @Composable
 fun AlbumsOverViewScreen(
-    albums : List<Album>,
+    albums: List<Album>,
     onPlayIconClick: (Album) -> Unit
 ) {
     val albumsStarting: MutableMap<Char, MutableList<Album>> = mutableMapOf()
+
     for (i in 0..25) {
         albumsStarting['A' + i] = mutableListOf()
     }
+    albumsStarting['#'] = mutableListOf()
 
-    for (i in 1..albums.size) {
-        albumsStarting[albums[i - 1].title[0]]?.add(albums[i-1])
+    for (album in albums) {
+        val title = album.title.trim()
+        val startingLetter = title.firstOrNull()?.uppercaseChar()?.takeIf { it.isLetter() } ?: '#'
+        albumsStarting[startingLetter]?.add(album)
     }
 
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        for (i in 0..25) {
-            val startingLetter: Char = ('A' + i)
-            if (albumsStarting[startingLetter]!!.size > 0) {
-                Column(modifier = Modifier
-                    .background(
-                        brush = ListenBrainzTheme.colorScheme.gradientBrush
-                    )
-                    .padding(top = 15.dp, bottom = 15.dp)) {
-                    Text(
-                        startingLetter.toString(),
-                        modifier = Modifier.padding(start = 10.dp, top = 10.dp, bottom = 5.dp),
-                        style = TextStyle(
-                            color = ListenBrainzTheme.colorScheme.lbSignature,
-                            fontSize = 20.sp,
-                            fontFamily = FontFamily(Font(R.font.roboto_bold)),
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(brush = ListenBrainzTheme.colorScheme.gradientBrush)
+    ) {
+        albumsStarting.forEach { (startingLetter, albumList) ->
+            if (albumList.isNotEmpty()) {
+                item {
+                    Column(
+                        modifier = Modifier.padding(top = 15.dp, bottom = 15.dp)
+                    ) {
+                        Text(
+                            startingLetter.toString(),
+                            modifier = Modifier.padding(start = 10.dp, top = 10.dp, bottom = 5.dp),
+                            style = TextStyle(
+                                color = ListenBrainzTheme.colorScheme.lbSignature,
+                                fontSize = 20.sp,
+                                fontFamily = FontFamily(Font(R.font.roboto_bold)),
+                            )
                         )
-                    )
-                    for (j in 1..albumsStarting[startingLetter]!!.size) {
-                        val coverArt = albumsStarting[startingLetter]!![j - 1].albumArt
-                        ListenCardSmall(
-                            trackName = albumsStarting[startingLetter]!![j - 1].title,
-                            artist = albumsStarting[startingLetter]!![j - 1].artist,
-                            coverArtUrl = coverArt,
-                            errorAlbumArt = R.drawable.ic_erroralbumart,
-                            modifier = Modifier.padding(
-                                start = 10.dp,
-                                end = 10.dp,
-                                top = 3.dp,
-                                bottom = 3.dp
-                            ),
-                            goToArtistPage = { onPlayIconClick(albumsStarting[startingLetter]!![j-1]) },
-                            onClick = { onPlayIconClick(albumsStarting[startingLetter]!![j-1]) }
-                        )
-
-                        Spacer(modifier = Modifier.height(10.dp))
                     }
+                }
+
+                items(albumList) { album ->
+                    val coverArt = album.albumArt
+                    ListenCardSmall(
+                        trackName = album.title,
+                        artist = album.artist,
+                        coverArtUrl = coverArt,
+                        errorAlbumArt = R.drawable.ic_erroralbumart,
+                        modifier = Modifier.padding(start = 10.dp, end = 10.dp, top = 3.dp, bottom = 3.dp),
+                        goToArtistPage = { onPlayIconClick(album) },
+                        onClick = { onPlayIconClick(album) }
+                    )
+                    Spacer(modifier = Modifier.height(10.dp))
                 }
             }
         }
