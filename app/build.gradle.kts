a66362994386bc7013fed778d27afee39009ea2c
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2025-04-06 16:53:36 +0530

    disable proguard mapping upload for now

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -119,6 +119,8 @@ sentry {
     // this will upload your source code to Sentry to show it as part of the stack traces
     // disable if you don't want to expose your sources
     includeSourceContext.set(true)
+    // TODO: Enable when server upload body max size is increased.
+    autoUploadProguardMapping.set(false)
 }
 
 dependencies {
