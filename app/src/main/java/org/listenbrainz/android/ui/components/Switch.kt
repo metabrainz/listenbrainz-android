c3fb9a1f804a89361b10d8aeb4790f47c9665d6b
Hemang Mishra <hemangmishranew@gmail.com>
2025-06-15 13:47:29 +0530

    switched to material 3

--- app/src/main/java/org/listenbrainz/android/ui/components/Switch.kt
+++ app/src/main/java/org/listenbrainz/android/ui/components/Switch.kt
@@ -1,10 +1,13 @@
 package org.listenbrainz.android.ui.components
 
 import androidx.compose.runtime.Composable
-import androidx.compose.material.Switch
-import androidx.compose.material.SwitchDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun Switch(modifier: Modifier = Modifier, checked: Boolean, onCheckedChange: (Boolean) -> Unit)
@@ -13,13 +16,21 @@ fun Switch(modifier: Modifier = Modifier, checked: Boolean, onCheckedChange: (Bo
         modifier = modifier,
         checked = checked,
         onCheckedChange = { onCheckedChange(it) },
-        colors  = SwitchDefaults.colors(
+        colors  = SwitchDefaults.colors().copy(
             checkedThumbColor = Color(0xFF918EB0),
             checkedTrackColor = Color(0xFF353070),
             uncheckedThumbColor = Color(0xFFD9D9D9),
             uncheckedTrackColor= Color(0xFF8D8D8D),
-            checkedTrackAlpha = 1f,
-            uncheckedTrackAlpha = 1f,
         ),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewSwitch() {
+    val checkedState = remember { mutableStateOf(true) }
+    org.listenbrainz.android.ui.components.Switch(
+        checked = checkedState.value,
+        onCheckedChange = { checkedState.value = it }
     )
 }
