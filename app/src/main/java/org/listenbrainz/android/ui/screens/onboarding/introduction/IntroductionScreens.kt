3cbf3fdb507795bdfd72bd80d7cad72d53f7317c
Hemang Mishra <hemangmishranew@gmail.com>
2025-07-02 15:47:23 +0530

    Added bug report asset

--- app/src/main/java/org/listenbrainz/android/ui/screens/onboarding/introduction/IntroductionScreens.kt
+++ app/src/main/java/org/listenbrainz/android/ui/screens/onboarding/introduction/IntroductionScreens.kt
@@ -40,7 +40,6 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.rounded.ArrowBackIosNew
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
