c89a6460db296a3878ab68376b685cd58c509673
Hemang Mishra <hemangmishranew@gmail.com>
2025-07-06 21:25:38 +0530

    Added Login Screen layout

--- app/src/main/java/org/listenbrainz/android/ui/screens/profile/ListenBrainzWebClient.kt
+++ app/src/main/java/org/listenbrainz/android/ui/screens/profile/ListenBrainzWebClient.kt
@@ -5,14 +5,20 @@ import android.net.Uri
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import com.limurse.logger.Logger
+import org.listenbrainz.android.model.ResponseError
 import org.listenbrainz.android.util.Resource
 
+const val TAG = "ListenBrainzWebClient"
+
 class ListenBrainzWebClient(
+    private val username: String,
+    private val password: String,
     private val onLoad: (Resource<String>) -> Unit,
     private val onPageLoadStateChange: (Boolean) -> Unit,
 ) : WebViewClient() {
 
     private var attemptedSettingsNavigation = false
+    private var attemptRedirectToLoginEndpoint = false
 
     override fun onPageStarted(
         view: WebView?,
@@ -27,27 +33,44 @@ class ListenBrainzWebClient(
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
         onPageLoadStateChange(false)
-        Logger.d("ListenBrainzWebClient", "onPageFinished URL: $url")
+        Logger.d(TAG, "onPageFinished URL: $url")
 
         if (url == null) {
-            Logger.d("ListenBrainzWebClient", "URL is null")
+            Logger.d(TAG, "URL is null")
             return
         }
 
         val uri = Uri.parse(url)
 
-        Logger.d("ListenBrainzWebClient", "Host: ${uri.host}, Path: ${uri.path}")
+        Logger.d(TAG, "Host: ${uri.host}, Path: ${uri.path}")
+
+        attemptLogin(view, url, username, password) { error ->
+            onLoad(Resource.failure(error = ResponseError.BAD_REQUEST.apply {
+                actualResponse = error
+            }))
+        }
 
         if (uri.host == "listenbrainz.org") {
             when {
-                !attemptedSettingsNavigation -> {
-                    Logger.d("ListenBrainzWebClient", "Navigating to settings page")
-                    attemptedSettingsNavigation = true
-                    view?.loadUrl("https://listenbrainz.org/settings")
+                !attemptRedirectToLoginEndpoint ->{
+                    Logger.d(TAG, "Attempting to redirect to login endpoint")
+                    attemptRedirectToLoginEndpoint = true
+                    view?.loadUrl("https://listenbrainz.org/login/musicbrainz")
                 }
+                !attemptedSettingsNavigation -> {
+                    Logger.d(TAG, "Navigating to settings page")
+                    attemptedSettingsNavigation = true
+                    view?.postDelayed(
+                        {
+                            view.loadUrl("https://listenbrainz.org/settings")
+                        },
+                        /*delayMillis =*/ 1000
+                    )
+                }
+
                 uri.path?.contains("/settings") == true -> {
                     onLoad(Resource.loading())
-                    Logger.d("ListenBrainzWebClient", "On settings page, waiting to extract token...")
+                    Logger.d(TAG, "On settings page, waiting to extract token...")
 
                     view?.postDelayed(
                         /*action = */{
@@ -59,8 +82,9 @@ class ListenBrainzWebClient(
                                     token.isNotEmpty() && token != "not found" -> {
                                         onLoad(Resource.success(token))
                                     }
+
                                     else -> {
-                                        Logger.d("ListenBrainzWebClient", "Token not found or empty")
+                                        Logger.d(TAG, "Token not found or empty")
                                         onLoad(Resource.failure())
                                     }
                                 }
@@ -71,5 +95,51 @@ class ListenBrainzWebClient(
                 }
             }
         }
+    }
+
+    fun attemptLogin(
+        view: WebView?,
+        url: String?,
+        username: String,
+        password: String,
+        onError: (String) -> Unit
+    ) {
+        if (url == null || !url.contains("musicbrainz.org//login")) {
+            Logger.d(TAG, "Not on login page, cannot attempt login")
+            onError("Not on login page")
+            return
+        }
+        if (view == null) {
+            Logger.d(TAG, "WebView is null, cannot attempt login")
+            onError("WebView is null")
+            return
+        }
+        val loginScript = """
+            (function(){
+            try{
+            var formContainer = document.getElementById('page');
+            if(!formContainer) return "Error: Form container not found";
+            document.getElementById('id-username').value = '$username';
+            document.getElementById('id-password').value = '$password';
+            document.querySelector('form').submit();
+            return "Login submitted";
+            } catch (e){
+            return "Error: " + e.message;
+            }
+            })();
+        """.trimIndent()
+        view.postDelayed({
+            view.evaluateJavascript(loginScript,) { result ->
+                Logger.d(TAG, "Login attempt result: $result")
+                if (result == "\"Login submitted\"") {
+                    Logger.d(TAG, "Login script executed successfully")
+                } else {
+                    Logger.e(TAG, "Error executing login script: $result")
+                    onError("Error executing login script: $result")
+                }
+
+            }
+        },
+            1000)
     }
 }
