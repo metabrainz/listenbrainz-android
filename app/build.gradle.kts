17ea307e41418ce93e2af4a86e00c94410a7b895
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2025-07-27 14:55:40 +0530

    Prepare v2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "org.listenbrainz.android"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 61
-        versionName = "2.8.4"
+        versionCode = 62
+        versionName = "2.9.0"
         multiDexEnabled = true
         testInstrumentationRunner = "org.listenbrainz.android.di.CustomTestRunner"
         vectorDrawables {
