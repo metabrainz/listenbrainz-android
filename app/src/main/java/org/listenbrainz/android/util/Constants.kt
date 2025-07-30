45cc8e49408ffbaa73d867c43fc1abf738c3d272
Hemang Mishra <hemangmishranew@gmail.com>
2025-07-30 11:44:34 +0530

    updated oboarding flag

--- app/src/main/java/org/listenbrainz/android/util/Constants.kt
+++ app/src/main/java/org/listenbrainz/android/util/Constants.kt
@@ -13,7 +13,7 @@ object Constants {
     const val SPOTIFY_REDIRECT_URI = "org.listenbrainz.android://callback"
     const val LISTENBRAINZ_API_BASE_URL = "https://api.listenbrainz.org/1/"
     const val LISTENBRAINZ_BETA_API_BASE_URL = "https://beta-api.listenbrainz.org/1/"
-    const val ONBOARDING = "onboarding_lb"
+    const val ONBOARDING = "onboarding_lb_updated"
     const val ABOUT_URL = "https://listenbrainz.org/about"
     const val LB_BASE_URL = "https://listenbrainz.org/"
     const val MB_BASE_URL = "https://musicbrainz.org/"
