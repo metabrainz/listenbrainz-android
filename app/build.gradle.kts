e363b0bc718daeabfa6d4289d808f29716b650cf
Nirvan Jain <nirvanj73@gmail.com>
2025-12-13 12:27:42 +0530

    Added the shimmer library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -275,4 +275,5 @@ dependencies {
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.compose.shimmer)
 }
