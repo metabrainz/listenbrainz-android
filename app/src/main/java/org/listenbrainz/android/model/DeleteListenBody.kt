11a4c027a90710c1e2455aa9f551d01e67824232
dibyajyoti-mandal <cptpricecod098@gmail.com>
2025-12-21 23:45:18 +0530

    Modified listens service

--- app/src/main/java/org/listenbrainz/android/model/DeleteListenBody.kt
+++ app/src/main/java/org/listenbrainz/android/model/DeleteListenBody.kt
@@ -1,37 +1,11 @@
 package org.listenbrainz.android.model
 
 import com.google.gson.annotations.SerializedName
-import java.util.ArrayList
 
-class DeleteListenBody {
+class DeleteListenBody(
+    @SerializedName("listened_at")
+    val listenedAt: Long,
 
-    @SerializedName("listens")
-    var listens: MutableList<ListenToDelete> = ArrayList()
-
-    // Helper to easily add a listen to the list
-    fun addListen(timestamp: Long, recordingMsid: String): DeleteListenBody {
-        listens.add(ListenToDelete(timestamp, recordingMsid))
-        return this
-    }
-
-    override fun toString(): String {
-        return "DeleteListenBody{" +
-                "listens=" + listens +
-                '}'
-    }
-
-    class ListenToDelete(
-        @SerializedName("listened_at")
-        var listenedAt: Long,
-
-        @SerializedName("recording_msid")
-        var recordingMsid: String
-    ) {
-        override fun toString(): String {
-            return "ListenToDelete{" +
-                    "listenedAt=" + listenedAt +
-                    ", recordingMsid='" + recordingMsid + '\'' +
-                    '}'
-        }
-    }
-}
+    @SerializedName("recording_msid")
+    val recordingMsid: String
+)
