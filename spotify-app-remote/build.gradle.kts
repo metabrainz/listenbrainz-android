957a7db44e09c1f2d615b8c0cad2b0317ee570fc
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2026-01-17 16:53:41 +0530

    update spotify sdk and add auth sdks

--- spotify-app-remote/build.gradle.kts
+++ spotify-app-remote/build.gradle.kts
@@ -1,2 +1,4 @@
 configurations.maybeCreate("default")
-artifacts.add("default", file("spotify-app-remote-release-0.7.2.aar"))
+artifacts.add("default", file("spotify-app-remote-release-0.8.0.aar"))
+artifacts.add("default", file("spotify-auth-release-2.1.0.aar"))
+artifacts.add("default", file("spotify-auth-store-release-2.1.0.aar"))
