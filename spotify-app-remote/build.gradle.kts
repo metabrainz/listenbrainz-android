e5df213be27a79019ab275c4bd1e45c06129c053
Jasjeet Singh <98077881+07jasjeet@users.noreply.github.com>
2026-01-17 22:15:09 +0530

    remove auth sdks for spotify

--- spotify-app-remote/build.gradle.kts
+++ spotify-app-remote/build.gradle.kts
@@ -1,4 +1,2 @@
 configurations.maybeCreate("default")
 artifacts.add("default", file("spotify-app-remote-release-0.8.0.aar"))
-artifacts.add("default", file("spotify-auth-release-2.1.0.aar"))
-artifacts.add("default", file("spotify-auth-store-release-2.1.0.aar"))
