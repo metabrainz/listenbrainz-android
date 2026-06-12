ee7b43b5b45b0cd099efb6492dd474e95cd6bd08
Nirvan Jain <nirvanj73@gmail.com>
2026-06-12 17:01:54 +0530

    moved AlbumRepository.kt and its impl to shared module and registered it in SharedRepositoryModule

--- shared/src/commonMain/kotlin/org/listenbrainz/shared/di/SharedRepositoryModule.kt
+++ shared/src/commonMain/kotlin/org/listenbrainz/shared/di/SharedRepositoryModule.kt
@@ -3,10 +3,13 @@ package org.listenbrainz.shared.di
 import org.koin.dsl.module
 import org.listenbrainz.shared.repository.blog.BlogRepository
 import org.listenbrainz.shared.repository.blog.BlogRepositoryImpl
+import org.listenbrainz.shared.repository.album.AlbumRepository
+import org.listenbrainz.shared.repository.album.AlbumRepositoryImpl
 import org.listenbrainz.shared.repository.artist.ArtistRepository
 import org.listenbrainz.shared.repository.artist.ArtistRepositoryImpl
 
 val sharedRepositoryModule = module {
     single<BlogRepository> { BlogRepositoryImpl(get()) }
     single<ArtistRepository> { ArtistRepositoryImpl(get(),get(),get()) }
+    single<AlbumRepository> { AlbumRepositoryImpl(get(),get(),get()) }
 }
