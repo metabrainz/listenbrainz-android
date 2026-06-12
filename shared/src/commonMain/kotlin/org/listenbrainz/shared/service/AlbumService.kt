3c21f3e3e538c41bb914d5b12c507dd6cba00df8
Nirvan Jain <nirvanj73@gmail.com>
2026-06-12 17:00:38 +0530

    moved AlbumService to shared module and registered in SharedNetworkServiceModule

--- shared/src/commonMain/kotlin/org/listenbrainz/shared/service/AlbumService.kt
+++ shared/src/commonMain/kotlin/org/listenbrainz/shared/service/AlbumService.kt
@@ -1,8 +1,9 @@
-package org.listenbrainz.android.service
+package org.listenbrainz.shared.service
 
 import de.jensklingenberg.ktorfit.http.POST
 import de.jensklingenberg.ktorfit.http.Path
 import org.listenbrainz.shared.model.album.Album
+
 interface AlbumService {
     @POST("album/{album_mbid}/")
     suspend fun getAlbumData(@Path("album_mbid") albumMbid: String): Album
