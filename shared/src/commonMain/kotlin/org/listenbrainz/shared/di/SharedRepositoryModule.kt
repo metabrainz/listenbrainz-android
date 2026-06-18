b8cdeecaa53246d690a740266ff0159d463927c5
Nirvan Jain <nirvanj73@gmail.com>
2026-06-18 14:20:56 +0530

    moved PlaylistDataRepository and its impl to shared module and registered it under sharedRepositoryModule

--- shared/src/commonMain/kotlin/org/listenbrainz/shared/di/SharedRepositoryModule.kt
+++ shared/src/commonMain/kotlin/org/listenbrainz/shared/di/SharedRepositoryModule.kt
@@ -57,4 +57,5 @@ val sharedRepositoryModule = module {
     single<PlaylistDataRepository> { PlaylistDataRepositoryImpl(get(),get(),get(),get(named(IO_DISPATCHER))) }
     single<FeedRepository> { FeedRepositoryImpl(get()) }
     single<SocialRepository> { SocialRepositoryImpl(get(),get()) }
+    single<PlaylistDataRepository> { PlaylistDataRepositoryImpl(get(),get(),get(),get(named(IO_DISPATCHER))) }
 }
